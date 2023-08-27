9c810d924b2b9d7e71198a4ca98170529df40077
Pauloslemma <pauloslemma1994@gmail.com>
2023-08-27 08:30:01 -0700

    0-fizzbuzz.py

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -1,29 +1,36 @@
-#!/usr/bin/node
-/*
-    Print a square with the character #
+#!/usr/bin/python3
 
-    The size of the square must be the first argument
-    of the program.
-*/
+import sys
 
-if (process.argv.length <= 2) {
-    process.stderr.write("Missing argument\n");
-    process.stderr.write("Usage: ./1-print_square.js <size>\n");
-    process.stderr.write("Example: ./1-print_square.js 8\n");
-    process.exit(1);
-}
+def fizzbuzz(n):
+    """
+    FizzBuzz function prints numbers from 1 to n separated by a space.
 
-// Parse the size argument and ensure it's a valid number
-const size = parseInt(process.argv[2]);
+    - For multiples of three, print "Fizz" instead of the number.
+    - For multiples of five, print "Buzz".
+    - For numbers which are multiples of both three and five, print "FizzBuzz".
+    """
+    if n < 1:
+        return
 
-if (isNaN(size) || size <= 0) {
-    process.stderr.write("Invalid size argument. Please provide a positive number.\n");
-    process.exit(1);
-}
+    tmp_result = []
+    for i in range(1, n + 1):
+        if (i % 3 == 0) and (i % 5 == 0):
+            tmp_result.append("FizzBuzz")
+        elif i % 3 == 0:
+            tmp_result.append("Fizz")
+        elif i % 5 == 0:
+            tmp_result.append("Buzz")
+        else:
+            tmp_result.append(str(i))
+    print(" ".join(tmp_result))
 
-for (let i = 0; i < size; i++) {
-    for (let j = 0; j < size; j++) {
-        process.stdout.write("#");
-    }
-    process.stdout.write("\n");
-}
+if __name__ == '__main__':
+    if len(sys.argv) <= 1:
+        print("Missing number")
+        print("Usage: ./0-fizzbuzz.py <number>")
+        print("Example: ./0-fizzbuzz.py 89")
+        sys.exit(1)
+
+    number = int(sys.argv[1])
+    fizzbuzz(number)
